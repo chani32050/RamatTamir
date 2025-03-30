c3335c3db0394f3d9417ce5c5c4d0fd9e580ddc6
Rachel Elfenbein <elfen1124@gmail.com>
2025-03-30 22:59:05 +0300

    finish the routers

--- Routers/ActivityRouter.js
+++ Routers/ActivityRouter.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {getAll,getById,create,deleteById,update} = require("../Controllers/CleanerController")
+const {getAll,getById,create,deleteById,update} = require("../Controllers/ActivityController")
 
 router.post("/create",create)
 router.get("/getAll",getAll)
